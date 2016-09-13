8a15b190ee5b95dc3467984a2ffb9c3d3be92ce2
xu_pc <xu_pc@Ctrip.com>
2016-09-13 19:04:29 +0800

    add comment

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var crypto = require('crypto');
 var User = require('../models/use');
 var Post = require('../models/post');
+var Comment = require('../models/comment');
 var multer = require('multer');
 
 var storage = multer.diskStorage({
@@ -178,6 +179,26 @@ module.exports = function (app) {
                 error: req.flash('error').toString()
             });
         });
+    });
+    app.post('/u/:name/:day/:title', function (req, res) {
+       var date = new Date(),
+           time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+        var comment = {
+            name: req.body.name,
+            email: req.body.email,
+            website: req.body.website,
+            time: time,
+            content: req.body.content
+        };
+        var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+        newComment.save(function (err) {
+            if(err){
+                req.flash('error', err);
+                return res.redirect('back');
+            }
+            req.flash('success', '留言成功！');
+            res.redirect('back');
+        })
     });
     app.get('/edit/:name/:day/:title', checkLogin);
     app.get('/edit/:name/:day/:title', function (req, res) {
